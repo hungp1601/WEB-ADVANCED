ee729c1fad35100a9281cd4006cb06515b5f3d42
thuhang2510 <tunguyet2510@gmail.com>
2023-11-15 22:53:19 +0700

    add repository of role

---
+++ Repositories/Interfaces/IRoleRepository.cs
@@ -0,0 +1,9 @@
+﻿using btl_web.Models;
+
+namespace btl_web.Repositories.Interfaces
+{
+    public interface IRoleRepository
+    {
+        Role GetByName(string name);
+    }
+}
---
+++ Repositories/RoleRepository.cs
@@ -0,0 +1,20 @@
+﻿using btl_web.Models;
+using btl_web.Repositories.Interfaces;
+
+namespace btl_web.Repositories
+{
+    public class RoleRepository : IRoleRepository
+    {
+        private readonly BtlWebContext _context;
+
+        public RoleRepository(BtlWebContext context)
+        {
+            _context = context;
+        }
+
+        public Role GetByName(string name)
+        {
+            return _context.Roles.SingleOrDefault(u => u.Name.Equals(name));
+        }
+    }
+}
